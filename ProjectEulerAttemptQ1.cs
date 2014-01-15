bdb6eb763513ddd939982d1cbce8485446f6e0ee
WhitexMamba <moho401@gmaILcom>
2014-01-14 21:14:28 -0700

    Update ProjectEulerAttemptQ1.cs

--- ProjectEulerAttemptQ1.cs
+++ ProjectEulerAttemptQ1.cs
@@ -1,78 +1,63 @@
 using System;
 
-class Program
+class EulerProjectAttemptFINAL
 {
     static void Main()
     {
-        int[] aiMultsOfThree = new int[333];
-        int[] aiMultsOfFive = new int[200];
-        
+        const int iLimit = 1000;
+        const int iNumberOfThrees = (iLimit - 1) / 3;
+        const int iNumberOfFives = (iLimit - 1) / 5;
+        const int iRepeats = iLimit / 15;
+
+        int[] aiMultsOfThree = new int[iNumberOfThrees];
+        int[] aiMultsOfFive = new int[iNumberOfFives];
+
         int iFirstCount = 0;
-        while (iFirstCount < 333)
+        while (iFirstCount < iNumberOfThrees)
             {
-                aiMultsOfThree[iFirstCount] = 3 + (3 * iFirstCount); 
+                aiMultsOfThree[iFirstCount] = 3 + (3 * iFirstCount);
                 iFirstCount++;
             }
 
         int iSecondCount = 0;
-        while (iSecondCount < 200)
-            {
-                aiMultsOfFive[iSecondCount] = 5 + (5 * iSecondCount);
-                iSecondCount++;
-            }
+        while (iSecondCount < iNumberOfFives)
+        {
+            aiMultsOfFive[iSecondCount] = 5 + (5 * iSecondCount);
+            iSecondCount++;
+        }
 
         int iThirdCount = 0;
         int iThreeMultsSum = 0;
-        while (iThirdCount < 333)
-            {
-                iThreeMultsSum += aiMultsOfThree[iThirdCount] + aiMultsOfThree[(iThirdCount++)];
-                iThirdCount++;
-            }
+        while (iThirdCount < iNumberOfThrees)
+        {
+            iThreeMultsSum += aiMultsOfThree[iThirdCount];
+            iThirdCount++;
+        }
 
         int iFourthCount = 0;
         int iFiveMultsSum = 0;
-        while (iFourthCount < 200)
-            {
-                iFiveMultsSum += aiMultsOfThree[iFourthCount] + aiMultsOfFive[(iFourthCount++)];
-                iFourthCount++;
-            }
+        while (iFourthCount < iNumberOfFives)
+        {
+            iFiveMultsSum += aiMultsOfFive[iFourthCount];
+            iFourthCount++;
+        }
+
 
         int iResult = iFiveMultsSum + iThreeMultsSum;
 
         int iCheckFor15 = 0;
-        while (iCheckFor15 < 333)
-            { 
-                if (aiMultsOfThree[iCheckFor15] % 15 == 0)
-                {
-                    iResult = iResult - aiMultsOfThree[iCheckFor15];
-                    iCheckFor15++;
-                }
-                else
-                iCheckFor15++;
-            }
-
-        /*
-        iCheckFor15 = 0;
-        while (iCheckFor15 < 200)
+        while (iCheckFor15 < iNumberOfThrees)
         {
-            if (aiMultsOfFive[iCheckFor15] % 15 == 0)
+            if (aiMultsOfThree[iCheckFor15] % 15 == 0)
             {
-                iResult = iResult - aiMultsOfFive[iCheckFor15];
+                iResult = iResult - aiMultsOfThree[iCheckFor15];
                 iCheckFor15++;
             }
             else
-            iCheckFor15++;
+                iCheckFor15++;
         }
-        */   
-   
 
-        Console.WriteLine("The total sum of all the multiples of 3 and 5 below 1000 is: " + iResult);
-
-
-        //      To my newbie eyes I thought this would be able to initialize two arrays of lenth 333 and 200, 
-        //  one for the muultiples of 3 below 1000 and one for the multiples of 5 below 1000 and add all the elements
-        //  of each array to arrive with the correct result of the sum of all numbers below 1000 divisable by either 3 or 5.
+        Console.WriteLine("The Sum of all numbers divisible by 3 or 5 is " + iResult);
     }
 
 }
-
